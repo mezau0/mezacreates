80fb116bd7c1f7577cce93b76870e83b76d4bbb7
mezau0 <70231270+mezau0@users.noreply.github.com>
2023-10-22 16:40:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+
+  //close modal by clicking esc key. arrowleft and arrowright 
+  //would be for the next & prev keys, but I can't figure it out
+  document.addEventListener('keydown', function(event) {
+    const key = event.key; // const {key} = event; in ES6+
+    if (key === "Escape") {
+        closeModal();
+    }
+});
+  
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  }
+
+//REFRESH game from conceptober page
+function refreshPage() {
+  document.getElementById('MemoryGame').src = document.getElementById('MemoryGame').src
+}
